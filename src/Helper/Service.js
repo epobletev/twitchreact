0c3d03c75ea0e2400342727f5c60856257d7faa2
Emilio Poblete <em.poblete.v@gmail.com>
2021-02-25 22:43:53 -0300

    Re add config

--- src/Helper/Service.js
+++ src/Helper/Service.js
@@ -2,7 +2,11 @@ const axios = require('axios');
 const config = require('../Config/Config.json');
 const endpoint = config.endpoint
 const clientID = config.clID
+
+
+
 export const getStreamInfo = async () =>{
+    
     const authTok = 'Bearer lfbz5wh8hnrer1pcu251vntb2xsho0'
     try{
         let { data } = await axios({
